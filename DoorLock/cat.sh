416877de48c35c43dc968853b727d475c71c2ae2
ethanlink255 <ethanlink255@gmail.com>
2018-12-11 13:57:03 -0500

    Removed cat.sh

--- DoorLock/cat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while :	
-do
-	UID_LINE=$(nfc-poll | grep -oE "UID \(NFC.*")
-	UID_SPACES=$(echo $UID_LINE | sed -r "s_.*: ([0-9]+.*)_\1_g")
-	UFID=$(echo $UID_SPACES | sed "s_ __g")
-	python3 main.py $UFID
-done
