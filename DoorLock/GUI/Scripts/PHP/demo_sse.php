a7c54d143d085a7b02b7475a9f757819cdb913a3
ethanlink255 <ethanlink255@gmail.com>
2019-03-13 11:11:32 -0400

    Full checkmark support implemented

--- DoorLock/GUI/Scripts/PHP/demo_sse.php
+++ DoorLock/GUI/Scripts/PHP/demo_sse.php
@@ -2,7 +2,7 @@
     header('Content-Type: text/event-stream');
     header('Cache-Control: no-cache');
 
-    $time = date('r');
-    echo "data: The server time is: {$time} \n\n";
+    $time = "False";
+    echo "data: {$time} \n\n";
     flush();
     ?>
